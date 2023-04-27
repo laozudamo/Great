76ba613a19e6103f72d0fc5f4bf184257da10a80
wuyandi <wuyandi@432ew.con>
2023-04-27 10:56:13 +0800

    feat: 配置css module

---
+++ src/layout/index.jsx
@@ -0,0 +1,9 @@
+import style from "./index.scss";
+
+export function Layout() {
+  return (
+    <div>
+      <h1 className={style.name}>Layout</h1>
+    </div>
+  );
+}
--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(async () => ({
   plugins: [react()],
 
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  // prevent vite from obscuring rust errors
   clearScreen: false,
   // tauri expects a fixed port, fail if that port is not available
   server: {
@@ -14,7 +12,15 @@ export default defineConfig(async () => ({
     strictPort: true,
     host: "0.0.0.0"
   },
- 
+  css: {
+    modules: true,
+    preprocessorOptions: {
+      scss: {
+        // additionalData: `@import "./src/styles/variables.scss";`
+      }
+    }
+  },
+
   envPrefix: ["VITE_", "TAURI_"],
   build: {
     // Tauri supports es2021
